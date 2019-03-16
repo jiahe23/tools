5c372c316c7e3bf74351f26dad24a714e2041cb0
Jia He <jiahe@dhcp-72-150.caltech.edu>
2019-03-15 17:02:43 -0700

    pressure diag tan18

--- pressure_diag/pressuretool.py
+++ pressure_diag/pressuretool.py
@@ -1,20 +1,27 @@
 import numpy as np
 
-def pressure_diag():
-    def __init__(self):
-        self.z = z
-        self.updraft
+class pressure_diag():
+    def __init__(self,statsdata,alpha_b=1.0/3,alpha_d=0.375,r_d=500):
+        self.alpha_b = alpha_b
+        self.alpha_d = alpha_d
+        self.r_d = r_d
+        self.t = [it.data.tolist() for it in statsdata.groups['timeseries']['t']]
+        self.z = [iz.data.tolist() for iz in statsdata.groups['reference']['z']]
+        self.updraft_dyn_pressure = statsdata.groups['profiles']['updraft_dyn_pressure']
+        self.updraft_fraction = statsdata.groups['profiles']['updraft_fraction']
+        self.updraft_w = statsdata.groups['profiles']['updraft_w']
+        self.env_w = statsdata.groups['profiles']['env_w']
+        self.updraft_b = statsdata.groups['profiles']['updraft_b']
+        self.rho0 = statsdata.groups['reference']['rho0']
 
+    def mean_pz_sink(self):
+        avgp_z = np.apply_along_axis( np.gradient, 1, self.updraft_dyn_pressure, z )
+        a_z = np.apply_along_axis( np.gradient, 1, self.updraft_fraction, z )
+        return -(self.updraft_fraction * avgp_z - self.updraft_dyn_pressure * a_z)
 
+    def pressure_drag(self):
+        return -(self.updraft_w[:]-self.env_w[:])*abs(self.updraft_w[:]-self.env_w[:])*self.alpha_d*\
+               self.rho0*np.sqrt(self.updraft_fraction[:])/self.r_d
 
-def mean_pz(z, updraft_dyn_pressure, updraft_fraction):
-    avgp_z = np.apply_along_axis( np.gradient, 1, updraft_dyn_pressure, z )
-    a_z = np.apply_along_axis( np.gradient, 1, updraft_fraction, z )
-    return updraft_fraction * avgp_z - updraft_dyn_pressure * a_z
-
-def pressure_drag():
-
-
-def pressure_buoy():
-
-
+    def pressure_buoy(self):
+        return -self.alpha_b*self.rho0[:]*self.updraft_fraction[:]*self.updraft_b[:]
