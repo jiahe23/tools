4c9f0d5413202ecc53dcb030d4a9147075362082
Jia He <jiahe@dhcp-72-150.caltech.edu>
2019-03-19 16:02:18 -0700

    optimize pressure diag

--- LES_diag_tool/pressuretool.py
+++ LES_diag_tool/pressuretool.py
@@ -10,17 +10,16 @@ class pycles_pressure_diag():
         self.t = [it.data.tolist() for it in statsdata.groups['timeseries']['t']]
         self.z = [iz.data.tolist() for iz in statsdata.groups['reference']['z']]
 
-        # self.updraft_dyn_pressure = statsdata.groups['profiles']['updraft_dyn_pressure']
-        self.updraft_fraction = statsdata.groups['profiles']['updraft_fraction']
-        self.updraft_w = statsdata.groups['profiles']['updraft_w']
-        self.env_w = statsdata.groups['profiles']['env_w']
-        self.updraft_b = statsdata.groups['profiles']['updraft_b']
-        self.buoyancy_mean = statsdata.groups['profiles']['buoyancy_mean']
-        self.rho0 = statsdata.groups['reference']['rho0']
+        self.updraft_dyn_pressure = statsdata.groups['profiles']['updraft_dyn_pressure'][:].data
+        self.updraft_fraction = statsdata.groups['profiles']['updraft_fraction'][:].data
+        self.updraft_w = statsdata.groups['profiles']['updraft_w'][:].data
+        self.env_w = statsdata.groups['profiles']['env_w'][:].data
+        self.updraft_reletive_buoyancy = statsdata.groups['profiles']['updraft_b'][:].data-\
+                                         statsdata.groups['profiles']['buoyancy_mean'][:].data
+        self.rho0 = statsdata.groups['reference']['rho0'][:].data
 
     def avgp_z(self):
-        updraft_dyn_pressure = self.statsdata.groups['profiles']['updraft_dyn_pressure'][:].data
-        return np.apply_along_axis(np.gradient, 1, updraft_dyn_pressure, self.z)
+        return np.apply_along_axis(np.gradient, 1, self.updraft_dyn_pressure, self.z)
 
     def fraction_z(self):
         return np.apply_along_axis(np.gradient, 1, self.updraft_fraction, self.z)
@@ -31,10 +30,10 @@ class pycles_pressure_diag():
         return -( self.updraft_fraction * self.avgp_z() + self.updraft_dyn_pressure * self.fraction_z() )
 
     def pressure_drag(self):
-        return -(self.updraft_w[:]-self.env_w[:])*abs(self.updraft_w[:]-self.env_w[:])*self.alpha_d*\
-               self.rho0*np.sqrt(self.updraft_fraction[:])/self.r_d
+        return -(self.updraft_w-self.env_w)*abs(self.updraft_w-self.env_w)*self.alpha_d*\
+               self.rho0*np.sqrt(self.updraft_fraction)/self.r_d
 
     def pressure_buoy(self):
-        return -self.alpha_b*self.rho0[:]*self.updraft_fraction[:]*(self.updraft_b[:]-self.buoyancy_mean[:])
+        return -self.alpha_b*self.rho0*self.updraft_fraction*self.updraft_reletive_buoyancy
 
 
