c97f656ab26df32556d060bfdc703fe91f7f5ba6
Jia He <jiahe@dhcp-72-150.caltech.edu>
2019-03-20 08:38:42 -0700

    updraft mask update

--- LES_diag_tool/updraftvar.py
+++ LES_diag_tool/updraftvar.py
@@ -4,12 +4,12 @@ class updraft_analysis():
     def __init__(self,statsdata):
         self.statsdata = statsdata
 
-    def masked_by_updraft(self,var):
+    def masked_by_updraft(self,var,maskedby='updraft_fraction'):
         if not isinstance(var, np.ndarray):
             sys.exit('Input Must Be np.ndarray!')
         if np.shape(var) != np.shape(self.statsdata.groups['profiles']['updraft_fraction']):
             sys.exit('ERROR: Input Dimension not Match Updraft')
-        return np.ma.mask_where(self.statsdata.groups['profiles']['updraft_fraction'][:].data ==0,
+        return np.ma.mask_where(self.statsdata.groups['profiles'][maskedby][:].data ==0,
                                 var)
 
     def profile_timeave(self,varname,tidx):
