2f8277c4b01a3e86316b4c951e154b272f51a069
JH <jiahe.gatech@gmail.com>
2019-03-20 09:59:43 -0700

    updraft mask setting

--- LES_diag_tool/updraftvar.py
+++ LES_diag_tool/updraftvar.py
@@ -4,14 +4,25 @@ class updraft_analysis():
     def __init__(self,statsdata):
         self.statsdata = statsdata
 
-    def masked_by_updraft(self,var,maskedby='updraft_fraction'):
-        if not isinstance(var, np.ndarray):
+    def masked_by_updraft(self,data,maskedby='updraft_fraction'):
+        if not isinstance(data, np.ndarray):
             sys.exit('Input Must Be np.ndarray!')
-        if np.shape(var) != np.shape(self.statsdata.groups['profiles']['updraft_fraction']):
-            sys.exit('ERROR: Input Dimension not Match Updraft')
+        if np.shape(data) != np.shape(self.statsdata.groups['profiles'][maskedby]):
+            sys.exit('ERROR: Input Dimension not Match Mask')
         return np.ma.masked_where(self.statsdata.groups['profiles'][maskedby][:].data == 0,
-                                var)
+                                  data)
 
     def profile_timeave(self,varname,tidx):
+        maskvar = 'updraft_fraction'
+        if varname[-5:] == 'cloud':
+            maskvar = 'cloud_fraction'
+        if varname[:5] == 'cloud':
+            maskvar = 'cloud_fraction'
         data = self.statsdata.groups['profiles'][varname][:].data
+        return self.masked_by_updraft(data,maskedby=maskvar)[tidx,:].mean(axis=0)
+
+    def derived_timeave(self,data,tidx):
+        if not isinstance(data, np.ndarray):
+            sys.exit('Input Must Be np.ndarray!')
         return self.masked_by_updraft(data)[tidx,:].mean(axis=0)
+
